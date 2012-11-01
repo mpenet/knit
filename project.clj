a228981484611352610e8583a5a0ee096555f816
Max Penet <m@qbits.cc>
2012-11-01 12:30:42 +0100

    snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/knit "0.1.2"
+(defproject cc.qbits/knit "0.1.2-SNAPSHOT"
   :description "Thin wrapper around Java Executors/Threads"
   :url "https://github.com/mpenet/knit"
   :license {:name "Eclipse Public License"
