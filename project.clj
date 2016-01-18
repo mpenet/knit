2f38be5c5ca1ecd643099c2bb2daa3a6240cef8b
Max Penet <m@qbits.cc>
2016-01-18 13:53:36 +0100

    bump deps

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
-(defproject cc.qbits/knit "0.3.0"
+(defproject cc.qbits/knit "0.3.1"
   :description "Thin wrapper around Java Executors/Threads"
   :url "https://github.com/mpenet/knit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cc.qbits/commons "0.2.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
-  :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5  {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
-             :dev  {:dependencies [[codox "0.8.10"]]}
+  :dependencies [[org.clojure/clojure "1.8.0-RC5"]
+                 [cc.qbits/commons "0.4.5"]
+                 [org.clojure/core.async "0.2.374"]]
+  :profiles {:dev  {:dependencies []}
              :test {:dependencies []}}
   :codox {:src-dir-uri "https://github.com/mpenet/knit/blob/master/"
           :src-linenum-anchor-prefix "L"
